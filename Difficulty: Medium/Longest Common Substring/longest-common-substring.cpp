7ccbccc244adf64a75eb43b590eafe9bb1b62b0c
Vijay Kumar <97971494+VIJAYKUMAR2435@users.noreply.github.com>
2024-08-14 08:53:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -6,30 +6,26 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  int solve(string &s1,string &s2,int n,int m){
-      std::vector<std::vector<int>> dp(n + 1, std::vector<int>(m + 1, 0));
-    int maxLength = 0;
-
-    // Fill the dp array
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= m; ++j) {
-            if (s1[i - 1] == s2[j - 1]) {
-                dp[i][j] = 1 + dp[i - 1][j - 1];
-                maxLength = std::max(maxLength, dp[i][j]);
-            } else {
-                dp[i][j] = 0; // Reset if characters do not match
-            }
-        }
-    }
-
-    return maxLength;
-  }
-    int longestCommonSubstr(string s1, string s2){
-       int n=s1.size();
-       int m=s2.size();
-       
-         return solve(s1,s2,n,m);
-// your code here
+    int longestCommonSubstr(string str1, string str2) {
+        // your code here
+       int n=str1.size();
+       int m=str2.size();
+       int dp[n+1][m+1];
+       int longest=0;
+       for(int i=0;i<n+1;i++){
+           for(int j=0;j<m+1;j++){
+               dp[i][j]=0;
+           }
+       }
+       for(int i=1;i<n+1;i++){
+           for(int j=1;j<m+1;j++){
+               if(str1[i-1]==str2[j-1]){
+                   dp[i][j]=1+dp[i-1][j-1];
+                   longest=max(longest,dp[i][j]);
+               }
+           }
+       }
+       return longest;
     }
 };
 
